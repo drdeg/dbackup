a0c2fa806c558e5734ca7ab0ef7f8ddfc6882f48
David Degerfeldt <david@degerfeldt.se>
2021-12-19 19:52:45 +0100

    Bumped version to 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dbackup", # Replace with your own username
-    version="0.3.6",
+    version="0.4",
     author="David Degerfeldt",
     author_email="david@degerfeldt.se",
     description="My backup solution",
